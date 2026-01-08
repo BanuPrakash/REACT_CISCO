b7fa4839ffcd9b790563a6bdeb0897c6f423c4bf
Banuprakash <banuprakashc@yahoo.co.in>
2026-01-08 12:22:03 +0530

    react app

--- shopapp/src/components/product/ProductCard.tsx
+++ shopapp/src/components/product/ProductCard.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import type Product from '../../models/Product'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default function ProductCard() {
+
+// interface and type can be used to declare shape of object
+type Props = {
+  product: Product
+}
+
+// interface Props {
+//   product: Product
+// }
+export default function ProductCard({product} : Props) {
   return (
-    <div>ProductCard</div>
+    <div className='col-md-4 my-2'>
+    <Card style={{ width: '18rem' }} >
+      <Card.Img variant="top" src={product.image} />
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+      </Card.Body>
+      <Card.Footer>
+        Price : {product.price} &nbsp;
+        <Button>Add</Button>
+      </Card.Footer>
+    </Card>
+    </div>
   )
 }
--- shopapp/src/components/product/ProductList.tsx
+++ shopapp/src/components/product/ProductList.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react"
+import type Product from "../../models/Product";
+import ProductCard from "./ProductCard";
 
 export default function ProductList() {
+  let [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products?limit=5")
+    .then(response => setProducts(response.data));
+  }, []);
+
   return (
-    <div>ProductList</div>
+    <div className="row">
+      {
+        products.map(product => <ProductCard product={product} key={product.id}/>)
+      }
+    </div>
   )
 }
